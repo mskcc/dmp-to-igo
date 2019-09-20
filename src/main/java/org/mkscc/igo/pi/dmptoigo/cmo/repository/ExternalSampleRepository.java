836852e375bd7bb046a621935c703e656e144892
imprezka88 <imprezka88@gmail.com>
2019-09-20 17:19:58 -0400

    [dmp-to-igo-8] Populate bait version

--- src/main/java/org/mkscc/igo/pi/dmptoigo/cmo/repository/ExternalSampleRepository.java
+++ src/main/java/org/mkscc/igo/pi/dmptoigo/cmo/repository/ExternalSampleRepository.java
@@ -2,8 +2,12 @@ package org.mkscc.igo.pi.dmptoigo.cmo.repository;
 
 import org.mskcc.domain.external.ExternalSample;
 
+import java.util.List;
+
 public interface ExternalSampleRepository {
     boolean exists(String sampleId);
 
     void save(ExternalSample externalSample);
+
+    List<ExternalSample> getAll();
 }
